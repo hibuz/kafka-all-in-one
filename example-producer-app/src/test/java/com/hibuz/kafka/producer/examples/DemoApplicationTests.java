d356f4c1e9612cbe85af15d139366447d0cde317
Hibuz <38822671+hibuz@users.noreply.github.com>
2025-12-28 20:31:14 +0000

    feat: add producer app

---
+++ example-producer-app/src/test/java/com/hibuz/kafka/producer/examples/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibuz.kafka.producer.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
