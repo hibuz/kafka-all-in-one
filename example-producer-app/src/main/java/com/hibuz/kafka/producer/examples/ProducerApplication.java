2e3ee07f941336a725964e3f982a3b276bc4bafe
Hibuz <38822671+hibuz@users.noreply.github.com>
2025-12-28 22:54:34 +0000

    fix: serializer setting

--- example-producer-app/src/main/java/com/hibuz/kafka/producer/examples/ProducerApplication.java
+++ example-producer-app/src/main/java/com/hibuz/kafka/producer/examples/ProducerApplication.java
@@ -1,9 +1,13 @@
 package com.hibuz.kafka.producer.examples;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class ProducerApplication {
 
@@ -12,7 +16,8 @@ public class ProducerApplication {
   }
 
   @KafkaListener(id = "payment-debug-consumer", topics = "${spring.kafka.topic.avro}")
-  public void listen(Payment payment) {
-      System.out.println(payment);
+  public void listen(ConsumerRecord<String, Payment> record) {
+      Payment payment = record.value();
+      log.info("Received message key-{}: id-{}, amount-{}", record.key(), payment.getId(), payment.getAmount());
   }
 }
